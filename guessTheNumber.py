e54e00f0af0728232e1923b6a02a2980f5a54709
rimarks <mark.wan93@gmail.com>
2020-05-11 19:12:05 +1000

    Finished Reviewed Collatz. Up chapter 3

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,22 +1,24 @@
 # This is a guess the number game.
-import random
-secretNumber = random.randint(1, 20)
-print('I am thinking of a number between 1 and 20.')
+import random #Import random package so that we can use the randint function
+secretNumber = random.randint(1, 20) # Generates a random number between 1 and 20 and then assigns to the secretNumber
+
+print('I am thinking of a number between 1 and 20.') #prints a prompt for he user
 
 #Ask the player to guess 6 times.
-for guessesTaken in range(1, 7):
+for guessesTaken in range(1, 7):# Iniates a for loop until it is finally exited they have a total of 6 times
     print ('Take a guess.')
-    guess = int(input())
+    guess = int(input())  # Prompts user to input an integrer, will break if there is a different property, maybe error handling can be built in
 
-    if guess<secretNumber:
+    if guess < secretNumber: #If the guess is lower than the secretNumber, then output
         print('Your guess is too low.')
     elif guess > secretNumber:
-        print('Your guess is too high.')
+        print('Your guess is too high.') #else if the guess is higher then the secretNumber, then output
     else:
         break #This condition is the correct guess!
-
+    #For loops ends here when the correct guess is chosen. For loop also ends if the number of Guesses are more than 6.
     
-if guess == secretNumber:
+#
+if guess == secretNumber: # acts as a win or loss condition. If the resulting  guess variable is correct then win condition. else Lost.
     print('Good Job! You guessed my number in '+ str(guessesTaken) + ' guesses!')
 else:
     print ('Nope, The number I was thinking of was ' +str(secretNumber))
--- theCollatzSequence.py
+++ theCollatzSequence.py
@@ -1,15 +1,15 @@
 def collatz(number):
-    if number % 2== 0:
+    if number % 2== 0: #tests whether the number is even or not.
         
-        print(str(number //2 ))
-        return (number//2)
-    elif number % 2 == 1:
-        print(str(3*number+1))
+        print(str(number //2 )) #Divides the even number by 2, output is an integer.
+        return (number//2) #outputs number to fun
+    elif number % 2 == 1: #else if the number is odd then:
+        print(str(3*number+1)) # 3xnumber + 1
         return (3*number+1)
 
 print('Enter number (that is not 1):')
-temp = input()
-fun = int(temp)
+temp = input() #prompts user for input
+fun = int(temp) #converts input into integer
 
-while fun != 1:
-    fun = collatz(fun)
+while fun != 1: #runs a while loop fun is not equal to 1
+    fun = collatz(fun) # keeps running collatz function until it works
